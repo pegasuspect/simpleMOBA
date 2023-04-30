cb99d4785644216e6b7d4d7896ab2fabe5ecb390
Osman Sekerlen <1279406+pegasuspect@users.noreply.github.com>
2023-04-29 22:47:20 -0400

    added a socket server

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+let id = 0;
+let players = [];
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('position', player => {
+    let p = players.find(x => x.id === player.id);
+    if (p) {
+      p.x = player.x
+      p.y = player.y
+    } else {
+      console.error(`Player with ${player.id} not found! Recieved: `, typeof player, player);
+    }
+  });
+
+  players.push({ x: 0, y: 0, id });
+  io.emit('id', id);
+
+  id++;
+});
+
+setInterval(() => {
+  if (players.length) {
+    io.emit('position', players);
+  }
+}, 100);
+
+setInterval(()=> {
+  players.forEach(x=> console.log(x.x + " " + x.y + ", " + x.id))
+},1000)
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
